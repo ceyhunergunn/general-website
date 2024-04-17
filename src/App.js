69ec02d60919c86eba29a08af2d3599b335938f3
ceyhunergunn <ceyhunergun61@gmail.com>
2024-04-17 16:16:54 +0300

    app screen size changed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Menu from "./Sections/Menu";
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="App ">
+    <div className="App container">
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Main />
