6352108eb085c37bc651766aa364e3c5e43579ea
ceyhunergunn <ceyhunergun61@gmail.com>
2024-05-07 12:51:27 +0300

    bugs fixe

--- src/Sections/Portfolio.jsx
+++ src/Sections/Portfolio.jsx
@@ -220,7 +220,7 @@ const Portfolio = () => {
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
               <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
             </svg>
-            <img style={{ height: "80px" }} src={konut} alt="portfolio" />
+            <img style={{ height: "80px" }} src={restaurant} alt="portfolio" />
           </div>
           <div className="w-75 h-50 mx-auto portfolio-card-bottom">
             Restaurant App.
@@ -346,7 +346,7 @@ const Portfolio = () => {
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
               <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
             </svg>
-            <img style={{ height: "80px" }} src={restaurant} alt="portfolio" />
+            <img style={{ height: "80px" }} src={konut} alt="portfolio" />
           </div>
           <div className="w-75 h-50 mx-auto portfolio-card-bottom">
             House Tracking System
