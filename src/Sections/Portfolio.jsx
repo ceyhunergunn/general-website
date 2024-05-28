5d5b4c07fa73ed91d99843bbe75ac29ec233d4ff
ceyhunergunn <ceyhunergun61@gmail.com>
2024-05-28 13:50:29 +0300

    new portfolio items added

--- src/Sections/Portfolio.jsx
+++ src/Sections/Portfolio.jsx
@@ -37,6 +37,14 @@ import bank2 from "../assets/portfolio/bank3.png";
 import bank3 from "../assets/portfolio/bank4.png";
 import bank4 from "../assets/portfolio/bank5.png";
 
+import dashboard from "../assets/portfolio/dashboard.png";
+import dashboard1 from "../assets/portfolio/dashboard1.png";
+import dashboard2 from "../assets/portfolio/dashboard2.png";
+
+import movie from "../assets/portfolio/movie.png";
+import movie1 from "../assets/portfolio/movie1.png";
+import movie2 from "../assets/portfolio/movie2.png";
+
 const Portfolio = () => {
   const [project, setProject] = useState({ name: "", img: "", desc: "" });
   const [showDetailModal, setShowDetailModal] = useState(false);
@@ -308,6 +316,90 @@ const Portfolio = () => {
           </div>
           <div className="w-75 h-50 mx-auto portfolio-card-bottom">
             Real Time Location System
+          </div>
+        </div>{" "}
+        <div className="col-lg-4 mx-auto col-md-6 col-sm-6 p-3">
+          <div
+            className="w-75 h-50 mx-auto portfolio-card-top"
+            style={{ position: "relative" }}
+          >
+            <svg
+              style={{
+                position: "absolute",
+                top: "10",
+                right: "10",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                setProject({
+                  name: "Movie App.",
+                  img1: movie1,
+                  img2: movie2,
+                  img3: "",
+                  img4: "",
+                  img5: "",
+                  link: "https://movie-ui.hicci.com.tr/",
+                  techs: "HTML, CSS, Sass, Bootstrap, JavaScript, React",
+                  desc: "In this project, I developed a website that showcases all the features and details of movies. I handled both the design and coding aspects of this website.",
+                });
+                handleShowDetailModal();
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="rgba(19, 104, 112,1)"
+              class="bi bi-info-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+            </svg>
+            <img style={{ height: "80px" }} src={movie} alt="movie" />
+          </div>
+          <div className="w-75 h-50 mx-auto portfolio-card-bottom">
+            Movie App.
+          </div>
+        </div>{" "}
+        <div className="col-lg-4 mx-auto col-md-6 col-sm-6 p-3">
+          <div
+            className="w-75 h-50 mx-auto portfolio-card-top"
+            style={{ position: "relative" }}
+          >
+            <svg
+              style={{
+                position: "absolute",
+                top: "10",
+                right: "10",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                setProject({
+                  name: "Dashboard App.",
+                  img1: dashboard1,
+                  img2: dashboard2,
+                  img3: "",
+                  img4: "",
+                  img5: "",
+                  link: "https://dashboard-ui.hicci.com.tr/",
+                  techs: "HTML, CSS, TailwindCSS, JavaScript, React",
+                  desc: "In this project, I developed a website that showcases all the features and details of a company. I handled both the design and coding aspects of this website.",
+                });
+                handleShowDetailModal();
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="rgba(19, 104, 112,1)"
+              class="bi bi-info-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+            </svg>
+            <img style={{ height: "80px" }} src={dashboard} alt="dashboard" />
+          </div>
+          <div className="w-75 h-50 mx-auto portfolio-card-bottom">
+            Dashboard App.
           </div>
         </div>{" "}
         <div className="col-lg-4 mx-auto col-md-6 col-sm-6 p-3">
